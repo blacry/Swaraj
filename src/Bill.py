86616cd2e0c35e9016c7a7cad9dd8b71c204ba7e
blacry <kansalvivek24@gmail.com>
2024-12-09 20:57:30 +0530

    fixed some bugs in bill

--- src/Bill.py
+++ src/Bill.py
@@ -1,7 +1,6 @@
 # jai mata di
-def bill(cid = 1010101010,name ='vivek', email='vivek@gmail.com', phone = 4545454545, check_in = '2024-12-05', check_out='2024-12-15', priceOfOneDay=1500 , room_no=15):
-      
-      fh = open('bill-{}.txt'.format(cid),'w')
+def bill(cid,name, email, phone, check_in, check_out, priceOfOneDay , room_no):
+      fh = open('{}bill.txt'.format(cid),'w',encoding='UTF-8')
 
       import mysql.connector as sqlcon
       con=sqlcon.connect(host="localhost",user="root",passwd="12345",database='swaraj_hotel',auth_plugin="mysql_native_password")
@@ -13,25 +12,24 @@ def bill(cid = 1010101010,name ='vivek', email='vivek@gmail.com', phone = 454545
       con.commit()
 
       price = priceOfOneDay *(days[0][0])
-
+      
       a =('''
-★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
-            🌟  SWARAJ  HOTEL BILL  🌟          
-★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
-🙍‍♂️  Customer Name   : {}                         
-✉️  Email Address   : {}                            
-📞  Phone Number    : {}                            
-👣  Check-In Date   : {}                            
-🏁  Check-Out Date  : {}                            
-🛌  Room No.        : {}
-💰  Total Amount    : {}
-★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
-   ✨  Thank you for booking with SWARAJ!  ✨
-★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+     ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+     🌟  SWARAJ  HOTEL BILL  🌟
+     ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+     🙍‍♂️  Customer Name   : {}                            
+     ✉️  Email Address   : {}                            
+     📞  Phone Number    : {}                            
+     👣  Check-In Date   : {}                            
+     🏁  Check-Out Date  : {}                            
+     🛌  Room No.        : {}
+     💰  Total Amount    : {}
+     ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
+     ✨  Thank you for booking with SWARAJ!  ✨
+     ★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★★
      
      '''.format(name,email,phone,check_in,check_out,room_no,price))
       
       fh.write(a)
       fh.close()
-      
-      print('created secusess')
+
